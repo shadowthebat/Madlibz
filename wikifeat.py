8c8f3522c4b62beffd5c6725b08b8b3004a7f7cb
shadowthebat <10837037+shadowthebat@users.noreply.github.com>
2019-01-16 22:07:19 -0500

    add wikifeat

---
+++ wikifeat.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from difflib import get_close_matches
+
+
+source = requests.get('https://en.wikipedia.org/wiki/Main_Page')
+soup = BeautifulSoup(source.text, 'lxml')
+
+div = soup.find(id='mp-tfa')  # finds the div for featured article wikipedia front page
+divp = div.p  # isolates the target paragraph
+p = divp.text  # makes paragraph readable
+p = p[:-19]  # removes (Full Article) link at the end of the article
+thtml = divp.b  # title tags
+
+title = thtml.text  # title
+
+
+quotes = re.findall(r'"[^"]*"', p)
+
+
+def question():
+    answer = input('Fill in the blank: ')
+    lst = [title]
+    close = get_close_matches(answer.lower(), lst)
+    if answer.lower() == title.lower():
+        print('You win!')
+    elif close:
+        print(f'{answer.upper()} is worth half a point!\nWe were expecting {title.upper()}')
+    else:
+        print('You lose, game over.')
+
+
+if quotes[0][1:-1] == title:  # if wiki articles title is in quotation marks
+    newp = p[len(thtml.text)+2:]  # creates paragraph without title
+    blankspace = '_'*len(title)  # creates blankspace appropriate in length to missing title
+    print(f'\"{blankspace}\"{newp}')
+    print()
+    question()
+else:
+    newp = p[len(thtml.text):]  # creates paragraph without title
+    blankspace = '_'*len(title)  # creates blankspace appropriate in length to missing title
+    print(f'{blankspace}{newp}')
+    question()
